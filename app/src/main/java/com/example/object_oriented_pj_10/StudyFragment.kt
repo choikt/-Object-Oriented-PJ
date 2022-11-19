0c27567a5a50091291a66f5514fa1f4e85991648
Wang HoEun <hoeunwang@gmail.com>
2022-11-19 20:31:10 +0900

    fix: 스터디 타이머 버그 해결(최종)

--- app/src/main/java/com/example/object_oriented_pj_10/StudyFragment.kt
+++ app/src/main/java/com/example/object_oriented_pj_10/StudyFragment.kt
@@ -1,15 +1,12 @@
 package com.example.object_oriented_pj_10
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.os.SystemClock
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.example.object_oriented_pj_10.databinding.FragmentCookBinding
 import com.example.object_oriented_pj_10.databinding.FragmentStudyBinding
 
 class StudyFragment : Fragment() {
@@ -33,39 +30,54 @@ class StudyFragment : Fragment() {
         // Inflate the layout for this fragment
         return binding?.root
     }
-    override fun onViewCreated(mode:String){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // elapsedRealtime: 부팅 이후의 밀리초를 리턴 (절전 모드에서 보낸 시간 포함)
         // 사용자가 현재시간을 수정해도 영향 받지 않음
         binding?.startBtn?.setOnClickListener {
-            binding?.chronometer?.base = SystemClock.elapsedRealtime() + pauseTime
-            binding?.chronometer?.start()
-
-            //버튼 표시 여부 조정
-            binding?.stopBtn?.isEnabled = true
-            binding?.resetBtn?.isEnabled = true
-            binding?.startBtn?.isEnabled = false
+            startButton()
         }
 
         binding?.stopBtn?.setOnClickListener {
-            pauseTime = binding?.chronometer?.base!! - SystemClock.elapsedRealtime()
-            binding?.chronometer?.stop()
-
-            //버튼 표시 여부 조정
-            binding?.stopBtn?.isEnabled = false
-            binding?.resetBtn?.isEnabled = true
-            binding?.startBtn?.isEnabled = true
+            stopButton()
         }
 
 
         binding?.resetBtn?.setOnClickListener {
-            pauseTime = 0L
-            binding?.chronometer?.base = SystemClock.elapsedRealtime()
-            binding?.chronometer?.stop()
-
-            //버튼 표시 여부 조정
-            binding?.stopBtn?.isEnabled = false
-            binding?.resetBtn?.isEnabled = false
-            binding?.startBtn?.isEnabled = true
+            resetButton()
         }
     }
+
+    private fun resetButton() {
+        pauseTime = 0L
+        binding?.chronometer?.base = SystemClock.elapsedRealtime()
+        binding?.chronometer?.stop()
+
+        //버튼 표시 여부 조정
+        binding?.stopBtn?.isEnabled = false
+        binding?.resetBtn?.isEnabled = false
+        binding?.startBtn?.isEnabled = true
+    }
+
+    private fun stopButton() {
+        pauseTime = binding?.chronometer?.base!! - SystemClock.elapsedRealtime()
+        binding?.chronometer?.stop()
+
+        //버튼 표시 여부 조정
+        binding?.stopBtn?.isEnabled = false
+        binding?.resetBtn?.isEnabled = true
+        binding?.startBtn?.isEnabled = true
+    }
+
+    private fun startButton() {
+        binding?.chronometer?.base = SystemClock.elapsedRealtime() + pauseTime
+        binding?.chronometer?.start()
+
+        //버튼 표시 여부 조정
+        binding?.stopBtn?.isEnabled = true
+        binding?.resetBtn?.isEnabled = true
+        binding?.startBtn?.isEnabled = false
+    }
+
 }
