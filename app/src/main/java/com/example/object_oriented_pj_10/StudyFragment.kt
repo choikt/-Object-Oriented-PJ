ee8c4726485412df73b0a3e8eae8018e397cee61
Wang HoEun <hoeunwang@gmail.com>
2022-11-19 15:40:38 +0900

    feat: study timer UI 구현 완료

--- app/src/main/java/com/example/object_oriented_pj_10/StudyFragment.kt
+++ app/src/main/java/com/example/object_oriented_pj_10/StudyFragment.kt
@@ -2,22 +2,23 @@ package com.example.object_oriented_pj_10
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.SystemClock
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.object_oriented_pj_10.databinding.FragmentCookBinding
 import com.example.object_oriented_pj_10.databinding.FragmentStudyBinding
 
 class StudyFragment : Fragment() {
     var binding: FragmentStudyBinding?=null
-    lateinit var countDownTimer: CountDownTimer
+    //뒤로가기 버튼 누른 시각 저장
+    var initTime = 0L
 
-
-    var timeRunning = false
-    var firstState=false
-    var time =0L
-    var tempTime= 0L
-
+    //멈춘 시각 저장
+    var pauseTime = 0L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,83 +35,43 @@ class StudyFragment : Fragment() {
         return binding?.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding?.btnFirst?.setOnClickListener{
-            viewMode("start")
-            startStop()
-        }
-        binding?.stopBtn?.setOnClickListener{
-            startStop()
-        }
-        binding?.cancelBtn?.setOnClickListener{
-            viewMode("cancel")
-            stopTimer()
-        }
-
-    }
-
-
-
-
     private fun viewMode(mode:String){
-        /*firstState=true
 
-        if (mode=="start"){*/
-            binding?.settingLayout?.visibility= View.GONE
-            binding?.timerLayout?.visibility= View.VISIBLE
-     /*   }
-        else{
-            binding?.settingLayout?.visibility= View.VISIBLE
-            binding?.timerLayout?.visibility= View.GONE
-        }*/
-    }
-    private fun startStop(){
-        if(timeRunning){
-            stopTimer()
-        }
-        else{
-            startTimer()
-        }
-    }
-    private fun startTimer(){
-        if (firstState){
-            val sHour= binding?.hourEdit?.text.toString()
-            val sMin= binding?.minEdit?.text.toString()
-            val sSec= binding?.secEdit?.text.toString()
-            time =(sHour.toLong()*3600000)+(sMin.toLong()*60000)+(sSec.toLong()*1000)+1000
-        }
-        else{
-            time = tempTime
-        }
-        countDownTimer= object:CountDownTimer(time,1000){
-            override fun onTick(millisUnitFinshed: Long) {
-                tempTime=millisUnitFinshed
-                updateTime()
-            }
+        // elapsedRealtime: 부팅 이후의 밀리초를 리턴 (절전 모드에서 보낸 시간 포함)
+        // 사용자가 현재시간을 수정해도 영향 받지 않음
+        binding?.startBtn?.setOnClickListener {
+            binding?.chronometer?.base = SystemClock.elapsedRealtime() + pauseTime
+            binding?.chronometer?.start()
 
-            override fun onFinish() {}
-        }.start()
-        binding?.stopBtn?.text="일시정지"
-        timeRunning=true
-        firstState=false
-    }
-    private fun stopTimer(){
-        countDownTimer.cancel()
-        timeRunning=false
-        binding?.stopBtn?.text="계속"
+            //버튼 표시 여부 조정
+            binding?.stopBtn?.isEnabled = true
+            binding?.resetBtn?.isEnabled = true
+            binding?.startBtn?.isEnabled = false
+        }
 
-    }
-    private fun updateTime(){
-        val hour = tempTime/3600000
-        val min =tempTime%3600000/60000
-        val sec =tempTime%3600000%60000/1000
+        binding?.stopBtn?.setOnClickListener {
+            pauseTime = binding?.chronometer?.base!! - SystemClock.elapsedRealtime()
+            binding?.chronometer?.stop()
 
-        var timerLeftText="$hour :"
-        if(min<10)timerLeftText+="0"
-        timerLeftText+="$min :"
-        if(sec<10)timerLeftText+="0"
-        timerLeftText+="$sec"
-        binding?.timerText?.text=timerLeftText
+            //버튼 표시 여부 조정
+            binding?.stopBtn?.isEnabled = false
+            binding?.resetBtn?.isEnabled = true
+            binding?.startBtn?.isEnabled = true
+        }
+
+
+        binding?.resetBtn?.setOnClickListener {
+            pauseTime = 0L
+            binding?.chronometer?.base = SystemClock.elapsedRealtime()
+            binding?.chronometer?.stop()
+
+            //버튼 표시 여부 조정
+            binding?.stopBtn?.isEnabled = false
+            binding?.resetBtn?.isEnabled = false
+            binding?.startBtn?.isEnabled = true
+        }
+
+
+
     }
 }
